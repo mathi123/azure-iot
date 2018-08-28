8bad4065a52948f137552cec6f00bcc459512d8b
jen lisabeth <lisabeth.jen@hotmail.com>
2018-08-28 10:42:47 +0200

    Update 00_create_datastore.sh

--- 00_create_datastore.sh
+++ 00_create_datastore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE ROLE datastore_ro NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD '$DS_RO_PASS';
+psql -v ON_ERROR_STOP=1 --username "ckan" <<-EOSQL
+    CREATE ROLE datastore_ro NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD 'datastore';
     CREATE DATABASE datastore OWNER ckan ENCODING 'utf-8';
     GRANT ALL PRIVILEGES ON DATABASE datastore TO ckan;
 EOSQL

e73eb97918570864307266157dfbff513bc890a0
jen lisabeth <lisabeth.jen@hotmail.com>
2018-08-28 12:08:42 +0200

    Delete 00_create_datastore.sh

--- 00_create_datastore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "ckan" <<-EOSQL
-    CREATE ROLE datastore_ro NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD 'datastore';
-    CREATE DATABASE datastore OWNER ckan ENCODING 'utf-8';
-    GRANT ALL PRIVILEGES ON DATABASE datastore TO ckan;
-EOSQL

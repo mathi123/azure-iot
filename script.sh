d1decca1d1bff6c9e25cbe5c1126c853c96196b7
jen lisabeth <lisabeth.jen@hotmail.com>
2018-08-28 14:33:03 +0200

    create seperate script for enabling datapusher

--- script.sh
+++ script.sh
@@ -31,11 +31,3 @@ echo "DATASTORE_READONLY_PASSWORD=datastore" >> .env
 echo "HOST_FQN=$1" >> .env
 
 docker-compose up -d
-
-docker exec ckan /usr/local/bin/ckan-paster --plugin=ckan datastore set-permissions -c /etc/ckan/production.ini | docker exec -i postgresql psql -U ckan
-
-sudo sed -i 's/ckan.plugins = /ckan.plugins = datastore datapusher /g' /var/lib/docker/volumes/azure-iot_ckan_config/_data/production.ini
-
-sudo sed -i 's/#ckan.datapusher.formats/ckan.datapusher.formats/g' /var/lib/docker/volumes/azure-iot_ckan_config/_data/production.ini
-
-docker-compose restart ckan

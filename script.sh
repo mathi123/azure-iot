14bb017700aee1a22253ba07d16039e1f9ced23e
jen lisabeth <lisabeth.jen@hotmail.com>
2018-08-29 11:48:33 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -30,4 +30,23 @@ echo "DATASTORE_READONLY_PASSWORD=datastore" >> .env
 
 echo "HOST_FQN=$1" >> .env
 
-docker-compose up -d
+docker-compose up -d --build
+
+sleep 15
+
+until docker exec -it postgresql psql -U 'ckan' -c '\q'; do
+        >&2 echo "Postgres is unavailable - sleeping"
+        sleep 15
+done
+
+>&2 echo "Postgres is up - setting up ckan and enabling datapusher"
+
+sleep 15
+
+docker-compose restart ckan
+
+sleep 15
+
+chmod +x enable_datapusher.sh
+
+./enable_datapusher.sh

5488eb911608b1ba84a82b3f2f8813a9197a6716
Mathias Colpaert <mathias.colpaert@sirus.be>
2018-09-27 14:37:16 +0200

    pass organization correctly

--- script.sh
+++ script.sh
@@ -3,11 +3,13 @@
 repoUrl="$1"
 fqdn="$2"
 redis="$3"
+organization="$4"
 
 echo "Installing azure city platform"
 echo "  - Repo: $repoUrl"
 echo "  - Host: $fqdn"
 echo "  - Redis: $redis"
+echo "  - Organization: $organization"
 
 rm -R -f azureiotcity
 
@@ -15,4 +17,4 @@ git clone $repoUrl azureiotcity
 
 cd azureiotcity
 
-./scripts/install.sh $fqdn $redis
+./scripts/install.sh $fqdn $redis $organization

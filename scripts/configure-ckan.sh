6619ce39a10e0242ac8eb1e77a9d465e8c46ca13
Mathias Colpaert <mathias.colpaert@sirus.be>
2018-09-27 15:19:41 +0200

    sudo

--- scripts/configure-ckan.sh
+++ scripts/configure-ckan.sh
@@ -7,7 +7,7 @@ echo "Organisation: $organization"
 
 apiKey=`sudo docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckan user -c /etc/ckan/production.ini add cityadmin email=test@localhost password=tM5cMP123 | grep apikey | sed "s/'/\n/g" | sed -n '4p'`
 
-docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckan sysadmin -c /etc/ckan/production.ini add cityadmin
+sudo docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckan sysadmin -c /etc/ckan/production.ini add cityadmin
 
 data="{\"name\":\"$organization\"}"
 

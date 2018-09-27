dee6499cb8f0d6a3ed156c79d7d4e0fc60a37e5c
Mathias Colpaert <mathias.colpaert@sirus.be>
2018-09-27 11:34:14 +0200

    update

--- script.sh
+++ script.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 repoUrl="$1"
-repoName="$2"
-fqdn="$3"
-redis="$4"
+fqdn="$2"
+redis="$3"
 
 echo "Installing azure city platform"
 echo "  - Repo: $repoUrl"
-echo "  - Repo folder: $repoName"
 echo "  - Host: $fqdn"
 echo "  - Redis: $redis"
 
-git clone $repoUrl
+git clone $repoUrl azureiotcity
 
-cd $repoName $repoName
+cd azureiotcity
 
 ./install.sh $fqdn $redis

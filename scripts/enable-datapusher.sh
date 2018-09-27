fe58219e5b71ab997a45c6e13f4ce70b9589d1cd
Mathias Colpaert <mathias.colpaert@sirus.be>
2018-09-27 13:11:18 +0200

    -

--- scripts/enable-datapusher.sh
+++ scripts/enable-datapusher.sh
@@ -6,9 +6,9 @@ cd docker
 
 docker exec ckan /usr/local/bin/ckan-paster --plugin=ckan datastore set-permissions -c /etc/ckan/production.ini | docker exec -i postgresql psql -U ckan
 
-sudo sed -i 's/ckan.plugins = /ckan.plugins = datastore datapusher /g' /var/lib/docker/volumes/ckan_config/_data/production.ini
+sudo sed -i 's/ckan.plugins = /ckan.plugins = datastore datapusher /g' /var/lib/docker/volumes/docker_ckan_config/_data/production.ini
 
-sudo sed -i 's/#ckan.datapusher.formats/ckan.datapusher.formats/g' /var/lib/docker/volumes/ckan_config/_data/production.ini
+sudo sed -i 's/#ckan.datapusher.formats/ckan.datapusher.formats/g' /var/lib/docker/volumes/docker_ckan_config/_data/production.ini
 
 until docker exec -it postgresql psql -U 'ckan' -c '\q'; do
         >&2 echo "Postgres is unavailable - sleeping"

e78b073c8146c8fa6379a3a465fc098177d57854
jen lisabeth <lisabeth.jen@hotmail.com>
2018-10-29 11:31:09 +0100

    Fixing ARM template

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -30,27 +30,24 @@ docker-compose start ckan
 
 echo -n "Running CKAN Check..."
 
-docker logs -f --until 2m ckan 2>&1 | tee CKANLOGS.log;
+CKANLOGS="$(docker logs -f --until 2m ckan 2>&1)"
 
 while true; do
-        if grep -Fq "SUCCESS" CKANLOGS.log;
-        then
+        if [[ "$CKANLOGS" =~ "SUCCESS" ]]; then
             echo "."
-            rm CKANLOGS.log
             cd ..
             exit 0
         fi
 
-        if grep -Fq "sqlalchemy" CKANLOGS.log;
-        then
+        if [[ "$CKANLOGS" =~ "sqlalchemy" ]]; then
             echo "."
             echo "Restarting ckan container..."
             docker-compose restart ckan
             START=$(date +%Y-%m-%dT%H:%M:%S)
             echo -n "Running CKAN Check..."
         fi
-        
-        docker logs --since "$START" ckan 2>&1 | tee CKANLOGS.log
+
+        CKANLOGS="$(docker logs --since "$START" ckan 2>&1)"
         sleep 10
         echo -n "."
 done

733cd0b579ed54965c576a2383aff85a5d6fe46d
jen lisabeth <lisabeth.jen@hotmail.com>
2018-10-29 12:19:54 +0100

    Fix error 'the input device is not a TTY'

--- scripts/configure-ckan.sh
+++ scripts/configure-ckan.sh
@@ -5,9 +5,9 @@ organization="$1"
 echo "configuring CKAN"
 echo "Organisation: $organization"
 
-apiKey=`sudo docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckan user -c /etc/ckan/production.ini add cityadmin email=test@localhost password=tM5cMP123 | grep apikey | sed "s/'/\n/g" | sed -n '4p'`
+apiKey=$(sudo docker exec -T ckan /usr/local/bin/ckan-paster --plugin=ckan user -c /etc/ckan/production.ini add cityadmin email=test@localhost password=tM5cMP123 | grep apikey | sed "s/'/\n/g" | sed -n '4p')
 
-sudo docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckan sysadmin -c /etc/ckan/production.ini add cityadmin
+sudo docker exec -T ckan /usr/local/bin/ckan-paster --plugin=ckan sysadmin -c /etc/ckan/production.ini add cityadmin
 
 data="{\"name\":\"$organization\"}"
 

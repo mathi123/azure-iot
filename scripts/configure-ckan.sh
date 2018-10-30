ac913ad9ccd0e5c1b499dbe319e0ac9c785b294c
jen lisabeth <lisabeth.jen@hotmail.com>
2018-10-30 15:07:32 +0100

    Update configure-ckan.sh

--- scripts/configure-ckan.sh
+++ scripts/configure-ckan.sh
@@ -8,9 +8,10 @@ echo "Organisation: $organization"
 USERDATA="$(docker exec ckan /usr/local/bin/ckan-paster --plugin=ckan user -c /etc/ckan/production.ini add cityadmin email=test@localhost password=tM5cMP123)"
 echo "$USERDATA"
 
-apiKey="$( echo "$USERDATA" | grep -o apikey | sed "s/'/\n/g" | sed -n '4p')"
+apiKey="$( echo "$USERDATA" | cut -d'\n' -f 3 | sed "s/'/\n/g" | sed -n '4p')"
+echo "$apiKey"
 
-docker exec -it ckan /usr/local/bin/ckan-paster --plugin=ckan sysadmin -c /etc/ckan/production.ini add cityadmin
+docker exec ckan /usr/local/bin/ckan-paster --plugin=ckan sysadmin -c /etc/ckan/production.ini add cityadmin
 
 data="{\"name\":\"$organization\"}"
 

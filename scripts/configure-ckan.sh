2997b4a828789183ffb7ddb4aea5653e233f67f8
jen lisabeth <lisabeth.jen@hotmail.com>
2018-12-10 16:10:47 +0100

    Fixing APIkey Get

--- scripts/configure-ckan.sh
+++ scripts/configure-ckan.sh
@@ -5,9 +5,9 @@ organization="$1"
 echo "configuring CKAN"
 echo "Organisation: $organization"
 
-USERDATA="$(docker exec ckan /usr/local/bin/ckan-paster --plugin=ckan user -c /etc/ckan/production.ini add cityadmin email=test@localhost password=tM5cMP123)"
+USERDATA=$(sudo docker exec ckan /usr/local/bin/ckan-paster --plugin=ckan user -c /etc/ckan/production.ini add cityadmin email=test@localhost password=tM5cMP123)
 
-apiKey="$( echo "$USERDATA" | cut -d$'\n' -f 3 | sed "s/'/\n/g" | sed -n '4p')"
+apiKey=$( echo "$USERDATA" | cut -d$'\n' -f 4 | sed "s/'/\n/g" | sed -n '4p')
 echo "$apiKey"
 
 docker exec ckan /usr/local/bin/ckan-paster --plugin=ckan sysadmin -c /etc/ckan/production.ini add cityadmin

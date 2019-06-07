e1bef5eb92f3735c6822cda9f6d76465cbb1ff0d
Mathias Colpaert <mathias.colpaert@sirus.be>
2019-06-07 15:43:04 +0200

    update

---
+++ city-store.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Sirus City Store"
+echo "Installing APT package dependencies"
+
+apt-get update
+
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+apt-get update
+
+apt-get install -y docker-ce
+
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+echo "Done!"
